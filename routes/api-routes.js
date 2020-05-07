f8e177b28d6eba26393efd1d2296aa8b241c6d4f
Gibbyswag <will.gibson7@gmail.com>
2020-05-07 16:55:46 -0700

    added api-routes

---
+++ routes/api-routes.js
@@ -0,0 +1,63 @@
+const db = require("../models");
+
+module.exports = function (app) {
+    // GET routes
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+            .populate("exercises")
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+            .populate("exercises")
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    // POST route
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.find({})
+            .populate("exercises")
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    // PUT route
+    app.put("/api/workouts/:ID", (req, res) => {
+        db.Exercise.create(req.body)            
+            .then((data) => db.Workout.findOneAndUpdate(
+                { _id: req.params.id },
+            {
+                $push: {
+                    exercises: data._id
+                },
+                $inc: {
+                    totalDuration: data.duration
+                }
+            },
+            { new: true })
+            )
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+                
+    });
+
+};
