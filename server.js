3d8afdc6166189d9824ca798af1b40b9eacfeb54
Gibbyswag <will.gibson7@gmail.com>
2020-05-06 21:02:52 -0700

    starter files + server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/HTMLroutes.js")(app, path);
+require("./routes/APIroutes.js")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// Listen
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
