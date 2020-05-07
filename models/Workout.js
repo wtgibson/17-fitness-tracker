8e43481f887f033cbe5da646f7ad7f5ce5a57745
Gibbyswag <will.gibson7@gmail.com>
2020-05-06 21:18:02 -0700

    added js models

---
+++ models/Workout.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: Date,
+    
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0
+    }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
